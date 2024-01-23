b562665990a5ab5ddfd0f39168b6179c87699a99
uali992 <157522711+uali992@users.noreply.github.com>
2024-01-23 18:14:27 +0000

    Create calculator project.py

---
+++ calculator project.py
@@ -0,0 +1,14 @@
+Python 3.12.1 (v3.12.1:2305ca5144, Dec  7 2023, 17:23:38) [Clang 13.0.0 (clang-1300.0.29.30)] on darwin
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 
+>>> 
+>>> numb1 = input("Enter a number:")
+Enter a number:3
+>>> numb2 = input("Enter another number:")
+Enter another number:9
+>>> result = int(numb1) + int(numb2)
+>>> 
+>>> result = int(numb1) + int(numb2)
+>>> 
+>>> print(result)
+12
